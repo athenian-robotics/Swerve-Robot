cb8de7bd55e15d6cb5a86f4f39abf76b8aba67ad
JTkeio <jacobtkeio@gmail.com>
2021-12-01 16:21:20 -0800

    fix turn encoder offset code and lower PID

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -29,7 +29,7 @@ public class SwerveModule extends SubsystemBase {
     public final CANEncoder driveEncoder;
     public final DutyCycleEncoder turningEncoder;
 
-    private final PIDController drivePIDController = new PIDController(0.003, 0, 0);
+    private final PIDController drivePIDController = new PIDController(0.001, 0, 0);
     private final ProfiledPIDController turningPIDController
             = new ProfiledPIDController(0.5, 0, 0,
             new TrapezoidProfile.Constraints(MODULE_MAX_ANGULAR_VELOCITY, MODULE_MAX_ANGULAR_ACCELERATION));
@@ -53,7 +53,7 @@ public class SwerveModule extends SubsystemBase {
         // Set the distance per pulse for the drive encoder. We can simply use the
         // distance traveled for one rotation of the wheel divided by the encoder
         // resolution.
-        driveEncoder.setPositionConversionFactor(2 * Math.PI * WHEEL_RADIUS / 7.2);
+        driveEncoder.setPositionConversionFactor(2 * Math.PI * WHEEL_RADIUS / 7.2); //per rotation, not native units :p (we checked)
 
         // Set the distance (in this case, angle) per pulse for the turning encoder.
         // This is the the angle through an entire rotation (2 * wpi::math::pi)
@@ -65,12 +65,14 @@ public class SwerveModule extends SubsystemBase {
         turningPIDController.enableContinuousInput(-Math.PI, Math.PI);
     }
 
+    public double getTurnEncoderAngle() {return turningEncoder.getDistance() + turningOffset;}
+
     /**
      * Returns the current state of the module.
      *
      * @return The current state of the module.
      */
-    public SwerveModuleState getState() { return new SwerveModuleState(driveEncoder.getVelocity(), new Rotation2d(turningEncoder.getDistance() + turningOffset)); }
+    public SwerveModuleState getState() { return new SwerveModuleState(driveEncoder.getVelocity(), new Rotation2d(getTurnEncoderAngle())); }
 
     /**
      * Sets the desired state for the module.
@@ -78,11 +80,11 @@ public class SwerveModule extends SubsystemBase {
      * @param state Desired state with speed and angle.
      */
     public void setDesiredState(SwerveModuleState state) {
-        state = SwerveModuleState.optimize(state, new Rotation2d(turningEncoder.getDistance() + turningOffset)); //never rotate more than 90 degrees :)
+        state = SwerveModuleState.optimize(state, new Rotation2d(getTurnEncoderAngle())); //never rotate more than 90 degrees :)
 
         // Calculate the motor outputs from the PID controllers.
         final var driveOutput = drivePIDController.calculate(driveEncoder.getVelocity(), state.speedMetersPerSecond);
-        final var turnOutput = turningPIDController.calculate(turningEncoder.getDistance() + turningOffset, state.angle.getRadians());
+        final var turnOutput = turningPIDController.calculate(getTurnEncoderAngle(), state.angle.getRadians());
 
         driveMotor.set(driveOutput);
         turningMotor.set(turnOutput);
